8c73c16a763dd186dfcbd16a154a9704dbff8e7b
Yannis Maragos <ioannismaragos@gmail.com>
2020-09-13 16:53:19 +0300

    Initial commit for v1.0.0

--- src/main.c
+++ src/main.c
@@ -1,3 +1,24 @@
+/*
+ * Changelogs - A simple command line program to document all notable changes for a list of projects
+ * by Yannis Maragos - https://www.pulsarbytes.net
+ * 
+ * v1.0.0
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 only,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2020 Yannis Maragos
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -131,7 +152,7 @@ void header(void)
         system("clear");
     #endif
 
-    printf("Changelogs - v%s\n", VERSION);
+    printf("Changelogs\n");
     printf("----------------------------------------\n");
 }
 
